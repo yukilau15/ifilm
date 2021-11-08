555d2fe8c5e9525c656078933b60ab89515de3f5
yukilau15 <yukilau15@gmail.com>
2021-11-08 11:54:49 +0800

    publish

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5000;
 
 console.log(process.env.ALAS_URI);
 
