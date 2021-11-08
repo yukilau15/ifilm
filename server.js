875502a6c630a8cba8efe023cfe0760d75c6c524
yukilau15 <yukilau15@gmail.com>
2021-11-08 11:33:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ connection.once("open", () =>
   console.log("MongoDB connection established successfully!")
 );
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
 app.use("/users", require("./routes/users"));
 app.use("/films", require("./routes/films"));
 
